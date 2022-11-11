ed6d5bd7169cd8e365180865ec957b5fa78515e2
Arnav-M <75835285+Arnav-M@users.noreply.github.com>
2022-11-11 15:21:31 -0800

    Initial commit

---
+++ app/src/main/java/com/example/muscleappv1/Calibration.kt
@@ -0,0 +1,11 @@
+package com.example.muscleappv1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Calibration : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calibration)
+    }
+}
